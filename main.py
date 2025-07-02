a26554d814c0e2518e5f9c006640f7b54f79ee82
Restimon <nico060901@gmail.com>
2025-07-02 15:12:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
 import discord
-import os
 from discord.ext import commands
 from config import TOKEN
-import roulette  # roulette.py doit être dans le même dossier
+import roulette
 import profile
 import giveaway
 import hardroulette
 
-intents = discord.Intents.default()
-bot = commands.Bot(command_prefix="!")
+intents = discord.Intents.default()  # Crée les intents par défaut (ne contient pas les intents privilégiés)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    # Sync global : fonctionne sur tous les serveurs où le bot est invité
     await bot.tree.sync()
     print(f"✅ Connecté en tant que {bot.user} — commandes globales synchronisées.")
 
-# Enregistre les commandes
 roulette.setup(bot)
 profile.setup(bot)
 giveaway.setup(bot)
 hardroulette.setup(bot)
 
-# Démarre le bot
-TOKEN = os.getenv("TOKEN")
+bot.run(TOKEN)
