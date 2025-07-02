95499eea1bbb0996495f2a1fdb6f79d46257299c
Restimon <nico060901@gmail.com>
2025-07-02 15:35:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ import hardroulette
 import set_team
 import help
 
-intents = discord.Intents.default()  # Crée les intents par défaut (ne contient pas les intents privilégiés)
+intents = discord.Intents.default()  # Intents par défaut
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
     await bot.tree.sync()
     print(f"✅ Connecté en tant que {bot.user} — commandes globales synchronisées.")
+    
+    # Statut personnalisé : "Joue ton destin avec la roulette"
+    activity = discord.Game(name="Joue ton destin avec la roulette")
+    await bot.change_presence(status=discord.Status.online, activity=activity)
 
 roulette.setup(bot)
 profile.setup(bot)
